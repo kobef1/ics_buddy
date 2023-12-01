419961c32dda7b01645b56de72947e3caac53013
bebedog <114669809+bebedog@users.noreply.github.com>
2023-12-01 15:35:30 +0800

    - amodia's initial commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav_bar from './Components/navbar';
 import ICS from './Pages/ics_buddy';
 import { Form, Button, Input, Modal, Select, Divider, message, Row, Col, Card, Typography } from "antd";
-import { Content,Header } from 'antd/es/layout/layout';
+import { Content, Header } from 'antd/es/layout/layout';
+import { useEffect } from 'react';
 
 function App() {
   return (
-  
-    <><div><Nav_bar /></div>
-      <Content style={{ marginTop: "100px" }}><ICS /></Content></> 
+
+    <>
+      <div>
+        <Nav_bar />
+      </div>
+
+      <Content style={{ marginTop: "100px" }}>
+        <ICS />
+      </Content>
+    </>
   );
 }
 
