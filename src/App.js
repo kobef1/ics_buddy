26521cf71b097d9645f78fc905cb02d8d1cf0bca
grusnivis <kathryn.sigaya@gmail.com>
2023-12-12 11:23:13 +0800

    ongoing export to pdf function

--- src/App.js
+++ src/App.js
@@ -3,104 +3,163 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav_bar from './Components/navbar';
 import ICS from './Pages/ics_buddy';
-import { Form, Button, Input, Modal, Select, Divider, message, Row, Col, Card, Typography, Spin, Alert, Space } from "antd";
-import { Content, Header } from 'antd/es/layout/layout';
-import { useEffect, useState } from 'react';
+import {
+    Form,
+    Button,
+    Input,
+    Modal,
+    Select,
+    Divider,
+    message,
+    Row,
+    Col,
+    Card,
+    Typography,
+    Spin,
+    Alert,
+    Space
+} from "antd";
+import {Content, Header} from 'antd/es/layout/layout';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
-import { NavLink, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import {NavLink, Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import ICSLogs from './Components/ICSLogs';
+import {ConvertToPdf} from "./Components/ConvertToPdf";
+import ReactPDF, {PDFDownloadLink} from "@react-pdf/renderer";
+import {RetrieveLogsFromDatabase} from "./Components/RetrieveLogsFromDatabase";
 
 function App() {
-  const [myData, setMyData] = useState({})
-  const [fetching, setFetching] = useState(false)
-  const [disconnectedStatus, setDisconnectedStatus] = useState(false)
-  const [myTimeout, setMyTimeout] = useState(500)
+    const [myData, setMyData] = useState({})
+    const [fetching, setFetching] = useState(false)
+    const [disconnectedStatus, setDisconnectedStatus] = useState(false)
+    const [myTimeout, setMyTimeout] = useState(500)
 
-  const [logsVisibility, setLogsVisibility] = useState(false)
+    const [logsVisibility, setLogsVisibility] = useState(false)
+    const [myLogs, setMyLogs] = useState(null)
 
-  const handleOpenLog = () => {
-    setLogsVisibility(true)
-  }
+    const handleOpenLog = () => {
+        setLogsVisibility(true)
+    }
 
-  const handleCloseLog = () => {
-    setLogsVisibility(false)
-  }
+    const handleCloseLog = () => {
+        setLogsVisibility(false)
+    }
 
-  useEffect(() => {
-    const myPromise = () => {
-      return new Promise((resolve, reject) => {
-        if (!fetching) {
-          // Not fetching.
-          axios.get('http://localhost:8000/fetch')
-            .then((response) => {
-              resolve(response.data)
+
+    useEffect(() => {
+        const myPromise = () => {
+            return new Promise((resolve, reject) => {
+                if (!fetching) {
+                    // Not fetching.
+                    axios.get('http://localhost:8000/fetch')
+                        .then((response) => {
+                            resolve(response.data)
+                        })
+                        .catch((err) => reject(err.response.data.message))
+                        .finally(() => setFetching(false))
+                } else {
+                    reject('Another fetch is on-going!')
+                }
             })
-            .catch((err) => reject(err.response.data.message))
-            .finally(() => setFetching(false))
-        } else {
-          reject('Another fetch is on-going!')
         }
-      })
-    }
 
-    const intervalId = setInterval(() => {
-      if (!fetching) {
-        console.log(`Am I fetching? :`, fetching)
-        myPromise()
-          .then((response) => {
-            setMyData(response)
-            setDisconnectedStatus(false)
-            console.log(response)
-          })
-          .catch(err => {
-            setDisconnectedStatus(true)
-            console.log(err)
-          })
-      }
-    }, myTimeout)
+        const intervalId = setInterval(() => {
+            if (!fetching) {
+                console.log(`Am I fetching? :`, fetching)
+                myPromise()
+                    .then((response) => {
+                        setMyData(response)
+                        setDisconnectedStatus(false)
+                        console.log(response)
+                    })
+                    .catch(err => {
+                        setDisconnectedStatus(true)
+                        console.log(err)
+                    })
+            }
+        }, myTimeout)
 
-    return () => {
-      clearInterval(intervalId)
-      setFetching(false)
-    }
+        return () => {
+            clearInterval(intervalId)
+            setFetching(false)
+        }
 
-  }, [])
+    }, [])
 
-  return (
-    <>
-      <Router>
-      <div>
-        <Nav_bar handleOpenLog={handleOpenLog} />
-      </div>
+    //retrieve logs from db
+    useEffect(() => {
+        axios.get('http://localhost:8000/db')
+            .then((response) => {
+                // setMyLogs(response.data)
+                let events = response.data
+                let dataSource = events.map(({event, date, time}, i) => (
+                    {
+                        key: i,
+                        event: event,
+                        date: date,
+                        time: time
+                    }
+                ))
+                setMyLogs(dataSource)
+                console.log(JSON.stringify(dataSource))
+            })
+    })
+    //  setMyLogs(<RetrieveLogsFromDatabase/>)
 
-      {/* <ICSLogs open={logsVisibility} /> */}
-      <Modal
-        title='Logs'
-        open={logsVisibility}
-        onCancel={handleCloseLog}
-        width={1500}>
-        <ICSLogs />
-      </Modal>
+    return (
+        <>
+            <Router>
+                <div>
+                    <Nav_bar handleOpenLog={handleOpenLog}/>
+                </div>
 
-      <Content style={{ marginTop: "100px" }}>
-        <Spin spinning={disconnectedStatus} tip={
-          <>
-            <Row justify={'center'}>
-              Loading..
-            </Row>
-            <Row justify={'center'}>
-              Please make sure you are connected to the ICS-Buddy switch or router!
-            </Row>
-          </>
-        }>
-        <Routes>
-              <Route path='/' element={<ICS myData={myData} />} />
-        </Routes>
-        </Spin>
-        </Content>
-      </Router>
-    </>
-  );
+                {/* <ICSLogs open={logsVisibility} /> */}
+                <Modal
+                    title='Logs'
+                    open={logsVisibility}
+                    onCancel={handleCloseLog}
+                    width={1500}
+                    footer={[
+                        // <Button key='convert' onClick={() => (
+                        //     <PDFDownloadLink document={<ConvertToPdf />} fileName="somename.pdf">
+                        //         {({ blob, url, loading, error }) =>
+                        //             loading ? 'Loading document...' : 'Download now!'
+                        //         }
+                        //     </PDFDownloadLink>
+                        // )}>Convert Log to PDF</Button>,
+                        <PDFDownloadLink document={<ConvertToPdf logs={myLogs}/>} fileName="testdoc.pdf">
+                            {({blob, url, loading, error}) =>
+                                loading ? 'Loading document...' : 'Download now!'
+                            }
+                        </PDFDownloadLink>,
+
+                        <Button key="Close" onClick={handleCloseLog}>
+                            Close Log
+                        </Button>,
+                    ]}
+                >
+                    <ICSLogs/>
+                </Modal>
+
+                <Content style={{marginTop: "100px"}}>
+                    <Spin spinning={disconnectedStatus} tip={
+                        <>
+                            <Row justify={'center'}>
+                                Loading...
+                            </Row>
+                            <Row justify={'center'}>
+                                Please make sure you are connected to the ICS-Buddy switch or router!
+                            </Row>
+                        </>
+                    }>
+                        <Routes>
+                            <Route path='/' element={<ICS myData={myData}/>}/>
+                        </Routes>
+                    </Spin>
+                </Content>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/Components/ConvertToPdf.jsx
@@ -0,0 +1,140 @@
+import React, {useEffect} from 'react';
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import axios from "axios";
+import {RetrieveLogsFromDatabase} from "./RetrieveLogsFromDatabase";
+import {forEach} from "react-bootstrap/ElementChildren";
+
+// Create styles
+// const styles = StyleSheet.create({
+//     page: {
+//         flexDirection: 'row',
+//         backgroundColor: '#E4E4E4'
+//     },
+//     section: {
+//         margin: 10,
+//         padding: 10,
+//         flexGrow: 1
+//     }
+// });
+
+const styles = StyleSheet.create({
+    table: {
+        display: "table",
+        width: "auto",
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderRightWidth: 0,
+        borderBottomWidth: 0
+    },
+    tableRow: {
+        margin: "auto",
+        flexDirection: "row"
+    },
+    tableCol: {
+        width: "20%",
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderLeftWidth: 0,
+        borderTopWidth: 0
+    },
+    tableCell: {
+        margin: "auto",
+        marginTop: 5,
+        fontSize: 10
+    }
+});
+
+const ConvertToPdf = (props) => {
+    let x = documentFormat(props.logs)
+    let y = [
+        <View style={styles.tableCol}>
+            <Text style={styles.tableCell}>Time</Text>
+        </View>,
+        <View style={styles.tableCol}>
+            <Text style={styles.tableCell}>Time</Text>
+        </View>,
+        <View style={styles.tableCol}>
+            <Text style={styles.tableCell}>Time</Text>
+        </View>,
+        <View style={styles.tableCol}>
+            <Text style={styles.tableCell}>Time</Text>
+        </View>,
+        <View style={styles.tableCol}>
+            <Text style={styles.tableCell}>Time</Text>
+        </View>
+    ]
+    console.log("x: ", x)
+    return(
+        <Document>
+            <Page style={styles.body}>
+                <View style={styles.table}>
+                    <View style={styles.tableRow}>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}>ID</Text>
+                        </View>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}>Event</Text>
+                        </View>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}>Signal</Text>
+                        </View>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}>Date</Text>
+                        </View>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}>Time</Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.tableRow}>
+                        {y.forEach(element => {
+                            return element
+                        })}
+                    </View>
+
+
+                    {/*{x.forEach((element)=> {*/}
+                    {/*    console.log("element", element)*/}
+                    {/*    return <>element</>*/}
+                    {/*})}*/}
+                    {/*{()=>{*/}
+                    {/*   return( console.log(`Hello from ConvertToPDF.jsx`))*/}
+                    {/*}}*/}
+                </View>
+            </Page>
+        </Document>
+    )
+}
+const documentFormat = (logs) => {
+    let arrayText = []
+    for (let i = 0; i < logs.length; i++){
+        let currentLog = logs[i]
+
+        let logText = (
+            <View style={styles.tableRow}>
+                <View style={styles.tableCol}>
+                    <Text style={styles.tableCell}>{currentLog.id}</Text>
+                </View>
+                <View style={styles.tableCol}>
+                    <Text style={styles.tableCell}>{currentLog.event}</Text>
+                </View>
+                <View style={styles.tableCol}>
+                    <Text style={styles.tableCell}>{currentLog.signal}</Text>
+                </View>
+                <View style={styles.tableCol}>
+                    <Text style={styles.tableCell}>{currentLog.date}</Text>
+                </View>
+                <View style={styles.tableCol}>
+                    <Text style={styles.tableCell}>{currentLog.time}</Text>
+                </View>
+            </View>
+        )
+        arrayText.push(logText)
+    }
+    console.log("arrayText display: ", arrayText)
+    return arrayText
+    // return console.log("arrayText display: ", arrayText)
+}
+
+
+export {ConvertToPdf}
